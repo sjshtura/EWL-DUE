2ade0313dbe35d722fb381f201dc62bddc9d2b0d
sjshtura <shakhawat.turag@stud.uni-due.de>
2020-11-05 01:54:53 +0100

    updated

---
+++ Web Scraping/entsooecrawling.py
@@ -0,0 +1,27 @@
+
+from entsoe import EntsoePandasClient
+import pandas as pd
+
+client = EntsoePandasClient(api_key='c4d117a2-f140-492b-bc2b-d615ec5774f4')
+
+start = pd.Timestamp('20171201', tz='Europe/Brussels')
+end = pd.Timestamp('20180101', tz='Europe/Brussels')
+country_code = 'BE'  # Belgium
+
+# methods that return Pandas Series
+client.query_day_ahead_prices(country_code, start=start,end=end)
+client.query_load(country_code, start=start,end=end)
+client.query_load_forecast(country_code, start=start,end=end)
+client.query_generation_forecast(country_code, start=start,end=end)
+
+# methods that return Pandas DataFrames
+client.query_wind_and_solar_forecast(country_code, start=start,end=end, psr_type=None)
+client.query_generation(country_code, start=start,end=end, psr_type=None)
+client.query_installed_generation_capacity(country_code, start=start,end=end, psr_type=None)
+client.query_crossborder_flows('DE', 'DK', start=start,end=end)
+client.query_imbalance_prices(country_code, start=start,end=end, psr_type=None)
+client.query_unavailability_of_generation_units(country_code, start=start,end=end, docstatus=None)
+#client.query_withdrawn_unavailability_of_generation_units('DE', start=start,end=end)
+
+ts = client.query_day_ahead_prices(country_code, start=start, end=end)
+ts.to_csv('outfile.csv')
